6c6c0e86353d346899be3c2d9fc8bf4bb4e5864e
kavangajera <145152936+kavangajera@users.noreply.github.com>
2025-05-07 21:52:03 +0530

    opening port

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from routes.train import train_bp
 from routes.test import test_bp
+import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all domains
@@ -16,4 +17,5 @@ def welcome():
     return jsonify({"message": "Welcome to watchdog"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use PORT from environment or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
