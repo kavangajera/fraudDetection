7a52d074bb4b7d448c118fe3f481e75ff246243c
kavangajera <145152936+kavangajera@users.noreply.github.com>
2025-05-09 18:09:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from flask_cors import CORS
 from routes.train import train_bp
 from routes.test import test_bp
 from routes.dummy_bank import bank_bp
+from werkzeug.middleware.proxy_fix import ProxyFix
 import os
 
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Set max content length to 16MB
+
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
 CORS(app, resources={r"/*": {"origins": "*"}})  # Enable CORS for all domains
 
 # Register Blueprints under "/api"
@@ -14,10 +17,13 @@ app.register_blueprint(train_bp, url_prefix='/api/train')
 app.register_blueprint(test_bp, url_prefix='/api/test')
 app.register_blueprint(bank_bp, url_prefix='/api/bank')
 
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
 # Root route
 @app.route('/', methods=['GET'])
 def welcome():
     return jsonify({"message": "Welcome to watchdog"}), 200
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5005))  # Use PORT from environment or default to 5000
